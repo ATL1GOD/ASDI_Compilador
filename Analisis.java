e4c403a3ba72b63e48debf4ced67c33139cbd673
Atl Cardoso <68315342+ATL1GOD@users.noreply.github.com>
2023-11-22 15:55:05 -0600

    Se inicio la clase Analisis

---
+++ Analisis.java
@@ -0,0 +1,11 @@
+public class Analisis {
+    // Método para ver si M[X, a] = X
+    public static String validar(String stringX, String stringA, String[][] tabla) {
+        int fila = 0;
+        int columna = 0;
+        for (int i = 0; i < tabla.length; i++) {
+            if (tabla[i][0].hashCode() == stringX.hashCode()) {
+                fila = i;
+                break;
+            }
+        }
