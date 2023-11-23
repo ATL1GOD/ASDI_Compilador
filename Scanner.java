cd938e539ef69f0a686e04393c1943b3815f342a
Atl Cardoso <atl1cardoso0@gmail.com>
2023-11-23 00:00:32 -0600

    Se creo el Scanner y se comenzo a codificar

--- Scanner.java
+++ Scanner.java
@@ -1,7 +1,21 @@
-import java.util.*; 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 /*Generalizamos la libreria util para evitar renglones*/
-public class Scanner{
+
+public class Scanner {
+
   private final String source;
+
   private final List<Token> tokens = new ArrayList<>();
-  private static final
+
+  private static final Map<String, TipoToken> palabrasReservadas;
+  static {
+    palabrasReservadas = new HashMap<>();
+    palabrasReservadas.put("select", TipoToken.SELECT);
+    palabrasReservadas.put("from", TipoToken.FROM);
+    palabrasReservadas.put("distinct", TipoToken.DISTINCT);
+  }
+
 }
