360296c29ff7541df4632640e179d44fc848039c
Atl Cardoso <atl1cardoso0@gmail.com>
2023-11-24 01:13:31 -0600

    Se realizo la lectura de la tabla

--- lectura.java
+++ lectura.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
 
+public class lectura {
+    public static ArrayList<String> aArraylist(String produccion) { /*
+                                                                     * Almacenará las palabras extraídas de la cadena de
+                                                                     * entrada.
+                                                                     */
+        ArrayList<String> palabras = new ArrayList<>();
+        String palabra = "";
+        for (int i = 5; i < produccion.length(); i++) {
+            if (i == 5 && produccion.charAt(i) == 32) {
+                continue;
+            } else {
+                if (produccion.charAt(i) != 32) {
+                    palabra += produccion.charAt(i);
+                } else {
+                    palabras.add(palabra);
+                    palabra = "";
+                }
+            }
+        }
+        palabras.add(palabra);
+        return palabras;
+    }
+
+}
